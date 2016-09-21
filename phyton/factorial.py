c994b052894d10bcc1e055aa0eb355712a59f0eb
RodrigoPerezG <rodrigoupiicsa@outlook.com>
2016-09-21 10:56:02 -0500

    Update factorial.py

--- phyton/factorial.py
+++ phyton/factorial.py
@@ -2,6 +2,7 @@ def factorial (numero)
     print numero 
     if numero ==0:
         return 1
+        ya esta mal tu codio mataviejas 
     else:
       return numero+factorial(numero-2)
 factorial (12)
